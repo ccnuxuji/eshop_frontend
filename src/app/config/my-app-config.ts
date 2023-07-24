d3b0d226245ee1e64f871fceb8a9628656424729
ccnuxuji <ccnuxuji.com>
2023-07-24 13:54:07 -0700

    init

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oaafj6fj16WxLQO15d7',
+        issuer: 'https://dev-93479268.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
