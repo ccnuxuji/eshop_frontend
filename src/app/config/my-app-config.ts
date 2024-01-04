bb5be36a630e6107a8836c18cf329acc8c82c1cb
Ji Xu <ccnuxuji@gmail.com>
2024-01-03 18:01:34 -0800

    change domain name

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -4,7 +4,7 @@ export default {
         clientId: '0oaafj6fj16WxLQO15d7',
         issuer: 'https://dev-93479268.okta.com/oauth2/default',
         // redirectUri: 'http://localhost:4200/login/callback',
-        redirectUri: 'https://www.ecommerce.jimmyxj.com/login/callback',
+        redirectUri: 'https://www.eshop.jimmyxj.com/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
